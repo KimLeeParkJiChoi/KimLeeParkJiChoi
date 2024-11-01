957bfcf6c8e418a595c02fdc05b397b184519b2f
coldrice99 <coldrice99@gmail.com>
2024-11-01 17:53:18 +0900

    init commit

---
+++ src/main/java/com/sparta/spangeats/SpangEatsApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spangeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpangEatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpangEatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/spangeats/SpangEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spangeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpangEatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
