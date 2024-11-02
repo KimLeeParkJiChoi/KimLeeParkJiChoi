bf29692eac3004fee2b43ed8382872fb834b1a79
tesbro1194 <tes1194@naver.com>
2024-11-02 18:13:27 +0900

    [#3] Fix: Review 엔티티
    - 타임스탬프 상속
    - Member 엔티티와 n:1 설정
    - 생성자 인자 수정

--- src/main/java/com/sparta/spangeats/domain/review/entity/Review.java
+++ src/main/java/com/sparta/spangeats/domain/review/entity/Review.java
@@ -1,5 +1,7 @@
 package com.sparta.spangeats.domain.review.entity;
 
+import com.sparta.spangeats.common.Timestamped;
+import com.sparta.spangeats.domain.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 @Table(name = "review")
-public class Review {
+public class Review extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -16,18 +18,21 @@ public class Review {
     private Long star;
     private String contents;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "user_id", nullable = false)
-    private Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
 
 //    @OneToOne(fetch = FetchType.LAZY)
 //    @JoinColumn(name = "todo_id", nullable = false)
     private Long orderId;
 
-    public Review(String contents, Long userId, Long orderId) {
-        this.contents = contents;
-        this.userId = userId;
+    private Long memberId;
+
+    public Review(Long memberId, Long orderId, Long star, String contents) {
+        this.memberId = memberId;
         this.orderId = orderId;
+        this.star = star;
+        this.contents = contents;
     }
 
     public void update(Long star, String contents) {
