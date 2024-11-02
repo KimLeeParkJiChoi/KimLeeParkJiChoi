ec106411510d783289f466cdcf27a0434ff1272a
tesbro1194 <tes1194@naver.com>
2024-11-02 18:16:32 +0900

    [#3] Feat: ReviewService
    - 생성(save) 기능 구현
    - 읽기(getAll) 기능 구현

--- src/main/java/com/sparta/spangeats/domain/review/service/ReviewService.java
+++ src/main/java/com/sparta/spangeats/domain/review/service/ReviewService.java
@@ -2,9 +2,14 @@ package com.sparta.spangeats.domain.review.service;
 
 import com.sparta.spangeats.domain.review.dto.ReviewRequestDto;
 import com.sparta.spangeats.domain.review.dto.ReviewResponseDte;
+import com.sparta.spangeats.domain.review.entity.Review;
 import com.sparta.spangeats.domain.review.repository.ReviewRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,17 +20,25 @@ public class ReviewService {
 
     private final ReviewRepository reviewRepository;
 
-
-    public void saveReview(String username, Long orderId, @Valid ReviewRequestDto requestDto) {
-
+    // 필터 구현 이후 userId - > member 로 수정해야함
+    public void saveReview(Long memberId, Long orderId, @Valid ReviewRequestDto requestDto) {
+        reviewRepository.save(new Review(memberId, orderId, requestDto.star(), requestDto.contents()));
     }
 
     public List<ReviewResponseDte> getALlForStore(Long storeId) {
         return null;
     }
 
-    public void getAllForMember(Long memberId) {
+    public Page<ReviewResponseDte> getAllForMember(int page, int size, String sortBy, boolean isAsc, Long memberId) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        Page<Review> reviewList = reviewRepository.findAllByMemberIdOrderByCreatedAtDesc(memberId, pageable);
+
+        return reviewList.map(review -> ReviewResponseDte.create(review));
     }
+
 
     public void update(Long reviewId, ReviewRequestDto requestDto) {
     }
