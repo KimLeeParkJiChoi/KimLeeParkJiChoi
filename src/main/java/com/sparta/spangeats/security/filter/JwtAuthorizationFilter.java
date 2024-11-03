821fecf234f8dd41d0d4b9f6b62974d77aa214ea
coldrice99 <coldrice99@gmail.com>
2024-11-03 14:56:34 +0900

    [#12] feat: JWT 인증 및 인가 필터 추가
    - JwtAuthenticationFilter: 로그인 시 JWT 생성 및 응답 헤더에 추가
    - JwtAuthorizationFilter: 요청 시 JWT 검증 및 사용자 인증 처리
    - UserDetailsImpl 및 UserDetailsServiceImpl 추가: 사용자 정보와 권한 관리 구현
    - 인증 필터와 인가 필터를 통해 JWT 기반의 인증 및 인가 체계 구축

---
+++ src/main/java/com/sparta/spangeats/security/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,67 @@
+package com.sparta.spangeats.security.filter;
+
+import com.sparta.spangeats.security.JwtUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j(topic = "JWT 검증 및 인가")
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String tokenValue = jwtUtil.getJwtFromHeader(request);
+
+        if (StringUtils.hasText(tokenValue)) {
+
+            if (!jwtUtil.validateToken(tokenValue)) {
+                log.error("Token Error");
+                return;
+            }
+
+            Claims info = jwtUtil.getMemberInfoFromToken(tokenValue);
+
+            try {
+                setAuthentication(info.getSubject());
+            } catch (Exception e) {
+                log.error(e.getMessage());
+                return;
+            }
+
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    // 인증 처리
+    private void setAuthentication(String email) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication authentication = createAuthentication(email);
+    }
+
+    // 인증 객체 생성
+    private Authentication createAuthentication(String email) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
+}
