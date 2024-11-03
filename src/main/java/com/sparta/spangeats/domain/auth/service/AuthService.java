3f226eab681e976e62be070a32e523fe2fdc6024
coldrice99 <coldrice99@gmail.com>
2024-11-04 02:22:06 +0900

    [#12] feat: 회원가입 기능 구현
     - AuthController에 회원가입 API 엔드포인트(/api/auth/signup) 추가
     - 회원가입 요청 시, 유효성 검사(@Valid) 수행 및 JWT 발급
     - AuthService에서 회원정보 저장, 중복 이메일/닉네임 확인 로직 구현
     - CustomPasswordEncoder를 이용한 비밀번호 암호화 처리
     - JwtUtil을 통해 가입 완료 후 JWT 토큰 발급

--- src/main/java/com/sparta/spangeats/domain/auth/service/AuthService.java
+++ src/main/java/com/sparta/spangeats/domain/auth/service/AuthService.java
@@ -1,11 +1,52 @@
 package com.sparta.spangeats.domain.auth.service;
 
+import com.sparta.spangeats.domain.auth.dto.request.SignupRequestDto;
+import com.sparta.spangeats.domain.auth.dto.response.LoginResponseDto;
+import com.sparta.spangeats.domain.auth.exception.AuthException;
+import com.sparta.spangeats.domain.member.entity.Member;
+import com.sparta.spangeats.domain.member.enums.MemberRole;
+import com.sparta.spangeats.domain.member.repository.MemberRepository;
+import com.sparta.spangeats.security.config.CustomPasswordEncoder;
+import com.sparta.spangeats.security.config.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class AuthService {
+    private final MemberRepository memberRepository;
+    private final CustomPasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    @Transactional
+    public LoginResponseDto signup(SignupRequestDto requestDto) {
+        if (memberRepository.existsByEmail(requestDto.email())) {
+            throw new AuthException("이미 존재하는 이메일 입니다.");
+        }
+
+        if (memberRepository.existsByNickname(requestDto.nickname())) {
+            throw new AuthException("이미 존재하는 닉네임 입니다.");
+        }
+
+        String encodedPassword = passwordEncoder.encode(requestDto.password());
+
+        MemberRole memberRole = MemberRole.of(requestDto.memberRole());
+
+        // 사용자 등록
+        Member member = new Member(requestDto.email(),
+                encodedPassword,
+                requestDto.nickname(),
+                memberRole,
+                requestDto.phoneNumber());
+        memberRepository.save(member);
+
+        String bearerToken = jwtUtil.createToken(member.getEmail(), member.getMemberRole());
+
+        return new LoginResponseDto(bearerToken);
+    }
+
 }
