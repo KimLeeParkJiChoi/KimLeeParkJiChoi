6bf8d837e70407180f921f04f43f734a7b4b9072
bbobsang <bbobsang@naver.com>
2024-11-04 17:02:53 +0900

    [#6] Feat: Menu, 가게&장바구니&주문과 연관관계 설정
    (장바구니와 주문은 주석처리)

--- src/main/java/com/sparta/spangeats/domain/menu/exception/InvalidMenuStatusException.java
+++ src/main/java/com/sparta/spangeats/domain/menu/exception/InvalidMenuStatusException.java
@@ -1,6 +1,6 @@
 package com.sparta.spangeats.domain.menu.exception;
 
-// 유효하지 않은 메뉴 상태일 때 발생하는 예외.
+// 유효하지 않은 메뉴 상태일 때 발생하는 예외
 public class InvalidMenuStatusException extends CustomException {
     public InvalidMenuStatusException(String status) {
         super("유효하지 않은 메뉴 상태: " + status);
--- src/main/java/com/sparta/spangeats/domain/menu/exception/MenuAlreadyExistsException.java
+++ src/main/java/com/sparta/spangeats/domain/menu/exception/MenuAlreadyExistsException.java
@@ -1,6 +1,6 @@
 package com.sparta.spangeats.domain.menu.exception;
 
-// 동일한 이름의 메뉴가 이미 존재할 때 발생하는 예외.
+// 동일한 이름의 메뉴가 이미 존재할 때 발생하는 예외
 public class MenuAlreadyExistsException extends CustomException {
     public MenuAlreadyExistsException(String menuName) {
         super("메뉴 이름 '" + menuName + "'은(는) 이미 존재합니다.");
