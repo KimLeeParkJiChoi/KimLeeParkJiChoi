0bfe39b99b9f5029dc2acb1247b3c10e226c58e5
bbobsang <bbobsang@naver.com>
2024-11-05 15:49:06 +0900

    [#16] Feat: InvalidMenuDescriptionException 추가
    메뉴 접근 권한 예외 설정

--- src/main/java/com/sparta/spangeats/domain/menu/exception/InvalidMenuAccessException.java
+++ src/main/java/com/sparta/spangeats/domain/menu/exception/InvalidMenuAccessException.java
@@ -1,8 +1,12 @@
 package com.sparta.spangeats.domain.menu.exception;
 
-// 해당 가게의 메뉴에 접근할 수 없을 때 발생하는 예외
 public class InvalidMenuAccessException extends CustomException {
+
   public InvalidMenuAccessException() {
-    super("이 메뉴는 현재 선택한 가게에 속하지 않으므로 접근할 수 없습니다.");
+    super("메뉴 접근 권한이 없습니다.");
+  }
+
+  public InvalidMenuAccessException(String message) {
+    super(message);
   }
 }
