701a001d29434fde27050d9f15ac45bf724736a7
bbobsang <bbobsang@naver.com>
2024-11-06 11:24:58 +0900

    [#6] Fix: @Setter 주입

--- src/main/java/com/sparta/spangeats/domain/menu/entity/Menu.java
+++ src/main/java/com/sparta/spangeats/domain/menu/entity/Menu.java
@@ -6,9 +6,11 @@ import jakarta.persistence.*;
 import lombok.Getter;
 
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @Table(name = "menus")
 public class Menu extends Timestamped {
