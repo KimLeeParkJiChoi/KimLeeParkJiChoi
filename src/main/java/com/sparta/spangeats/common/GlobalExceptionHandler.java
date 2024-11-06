f9346cb576765c1700a5a19e6e7affd5745cccc9
shinheekim <shinhee1241@gmail.com>
2024-11-06 19:10:47 +0900

    [#76] refactor: 주소 생성 예외처리
    주소는 최대 10개까지만 생성할 수 있도록 설정하였고, 그에 따라 예외 처리를 GlobalExceptionHandler로 처리

--- src/main/java/com/sparta/spangeats/common/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/spangeats/common/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.sparta.spangeats.common;
 
+import com.sparta.spangeats.domain.address.exception.AddressOverCountException;
 import com.sparta.spangeats.domain.cart.exception.CartNotFoundException;
 import com.sparta.spangeats.domain.store.exception.StoreException;
 import org.springframework.http.HttpStatus;
@@ -20,5 +21,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponseDto> handleStoreExceptionCartNotFoundException(CartNotFoundException ex) {
         ErrorResponseDto errorResponse = new ErrorResponseDto(ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(AddressOverCountException.class)
+    public ResponseEntity<ErrorResponseDto> handleAddressOverCountException(AddressOverCountException ex) {
+        ErrorResponseDto errorResponse = new ErrorResponseDto(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
--- src/main/java/com/sparta/spangeats/domain/address/service/AddressService.java
+++ src/main/java/com/sparta/spangeats/domain/address/service/AddressService.java
@@ -1,12 +1,16 @@
 package com.sparta.spangeats.domain.address.service;
 
+import com.sparta.spangeats.domain.address.dto.AddressResponse;
 import com.sparta.spangeats.domain.address.dto.AddressSaveRequest;
 import com.sparta.spangeats.domain.address.entity.Address;
+import com.sparta.spangeats.domain.address.exception.AddressOverCountException;
 import com.sparta.spangeats.domain.address.repository.AddressRepository;
 import com.sparta.spangeats.domain.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +21,9 @@ public class AddressService {
 
     @Transactional
     public void create(AddressSaveRequest request, Member member) {
+        if (addressRepository.countByMemberId(member.getId()) >= 10) {
+            throw new AddressOverCountException("주소는 최대 10개까지 만들 수 있습니다.");
+        }
         Address address = Address.builder()
                 .address(request.address())
                 .member(member)
