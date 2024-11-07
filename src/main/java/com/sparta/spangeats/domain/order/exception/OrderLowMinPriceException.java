71c62ddb6ad50aa5d510ae6d71eaafc4e54db5ed
shinheekim <shinhee1241@gmail.com>
2024-11-08 00:00:48 +0900

    [#91] style: 불필요한 주석 및 import 제거

--- src/main/java/com/sparta/spangeats/domain/order/exception/OrderLowMinPriceException.java
+++ src/main/java/com/sparta/spangeats/domain/order/exception/OrderLowMinPriceException.java
@@ -1,9 +1,5 @@
 package com.sparta.spangeats.domain.order.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-//@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class OrderLowMinPriceException extends RuntimeException {
     public OrderLowMinPriceException(String message) {
         super(message);
--- src/main/java/com/sparta/spangeats/domain/order/exception/OrderNotOpenTimeException.java
+++ src/main/java/com/sparta/spangeats/domain/order/exception/OrderNotOpenTimeException.java
@@ -1,6 +1,5 @@
 package com.sparta.spangeats.domain.order.exception;
 
-//@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class OrderNotOpenTimeException extends RuntimeException {
     public OrderNotOpenTimeException(String message) {
         super(message);
